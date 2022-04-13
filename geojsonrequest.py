f2e025960a5d61718f5a6ea1c9ccdc4af398ec26
davidmarcos98 <davidmarcosg98@gmail.com>
2022-04-13 09:28:31 +0000

    use data instead of saving to thousands of files

--- geojsonrequest.py
+++ geojsonrequest.py
@@ -1,38 +1,43 @@
 import json
-import requests
-import urllib.parse
 import os
+import urllib.parse
+from multiprocessing.sharedctypes import Value
+
+import requests
+
+from settings import VARIABLES
 
 files_dir = 'geojson-files'
 images_dir = 'mapbox-images'
-# Paste in an access token here
-access_token = ""
+access_token = VARIABLES.get('mapbox_key')
+mapbox_username = VARIABLES.get('mapbox_username')
 
-def main():
-	index = 0
-	try: 
-		os.mkdir(f"./{images_dir}")
-	except OSError as error: 
-		print(error)
 
-	for filename in os.listdir(files_dir):
-		with open(f'./{files_dir}/{index:06}.geojson') as json_file:
-			data = json.load(json_file)
-			xcoord = data["coordinates"][0]
-			ycoord = data["coordinates"][1]
+def generate_images(style, images_data):
+    try:
+        os.mkdir(f"./{images_dir}")
+    except OSError as error:
+        print(error)
 
-			encodeddata = urllib.parse.quote(json.dumps(data), safe='')
+    for index, data in enumerate(images_data):
+        xcoord = data["coordinates"][0]
+        ycoord = data["coordinates"][1]
 
-			r = requests.get(f"https://api.mapbox.com/styles/v1/trekview/cl1nbz22z002614pn626zhm7u/static/geojson({encodeddata})/{xcoord}, {ycoord},17/500x300?access_token={access_token}")
-			
-			if r.status_code == 200:
-				with open(f"./{images_dir}/{index:06}.png", 'wb') as f:
-					f.write(r.content)
-					f.close()
-					print(f"Fetched image: {index}")
-			else:
-				print("Failed to fetch image with status code: ", r.status_code)
-		index += 1
+        encodeddata = urllib.parse.quote(json.dumps(data), safe='')
+
+        r = requests.get((
+            f"https://api.mapbox.com/styles/v1/{mapbox_username}/{style}/"
+            f"static/geojson({encodeddata})/{xcoord}, {ycoord},17/"
+            f"500x300?access_token={access_token}"
+        ))
+        if r.status_code == 200:
+            with open(f"./{images_dir}/{index:06}.png", 'wb') as f:
+                f.write(r.content)
+                f.close()
+                print(f"Fetched image: {index}")
+        else:
+            print("Failed to fetch image with status code: ", r.status_code)
+
 
 if __name__ == '__main__':
-	main()
+    generate_images()
